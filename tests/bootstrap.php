5f7575ca11f42c559f035885d6d6c972a8ddd496
David Sánchez <david.sanchez@deporvillage.com>
2023-06-27 17:23:01 +0200

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
+if(method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
